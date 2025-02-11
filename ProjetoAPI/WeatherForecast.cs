54c494dd1a725602cf2e3858b393df6bcaa65026
Prof. Cristiano <cristiano.paula@docente.senai.br>
2025-02-11 14:51:43 -0300

    Alterado o arquivo water

--- ProjetoAPI/WeatherForecast.cs
+++ ProjetoAPI/WeatherForecast.cs
@@ -9,5 +9,12 @@ namespace ProjetoAPI
         public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 
         public string? Summary { get; set; }
+
+        public WeatherForecast(DateOnly date, int temperatureC, string? summary)
+        {
+            Date = date;
+            TemperatureC = temperatureC;
+            Summary = summary;
+        }
     }
 }
